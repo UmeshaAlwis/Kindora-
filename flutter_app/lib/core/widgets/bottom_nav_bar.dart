2ef27f380bcaf818088bf4c30ba48e22c876c09e
UmeshaAlwis <uma20812ud@gmail.com>
2026-03-06 01:04:04 +0530

    Replace merch icon with svg heart icon

--- flutter_app/lib/core/widgets/bottom_nav_bar.dart
+++ flutter_app/lib/core/widgets/bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AppBottomNavBar extends StatelessWidget {
   final int currentIndex;
@@ -21,41 +22,42 @@ class AppBottomNavBar extends StatelessWidget {
       child: BottomNavigationBar(
         currentIndex: currentIndex,
         type: BottomNavigationBarType.fixed,
-
         selectedItemColor: const Color(0xFF0C0C79),
         unselectedItemColor: Colors.grey,
-
         elevation: 0,
         enableFeedback: false,
-
         onTap: onTap,
 
-        items: const [
-          BottomNavigationBarItem(
+        // ❗ removed const because SvgPicture is not const
+        items: [
+          const BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
             activeIcon: Icon(Icons.home),
             label: "Home",
           ),
 
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.rss_feed_outlined),
             activeIcon: Icon(Icons.rss_feed),
             label: "Feed",
           ),
 
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.send_outlined),
             activeIcon: Icon(Icons.send),
             label: "Messages",
           ),
 
           BottomNavigationBarItem(
-            icon: Icon(FeatherIcons.heart),
-            activeIcon: Icon(FeatherIcons.heart),
+            icon: SvgPicture.asset(
+              "assets/icons/heart.svg",
+              height: 24,
+              width: 24,
+            ),
             label: "Merch",
           ),
 
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.person_outline),
             activeIcon: Icon(Icons.person),
             label: "Profile",
