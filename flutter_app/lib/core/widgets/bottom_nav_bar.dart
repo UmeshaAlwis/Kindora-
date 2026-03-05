5312dc071669b0e00030a8a5e764d46a9e165f23
UmeshaAlwis <uma20812ud@gmail.com>
2026-03-06 00:50:27 +0530

    Disable ripple and highlight effect in bottom navigation

--- flutter_app/lib/core/widgets/bottom_nav_bar.dart
+++ flutter_app/lib/core/widgets/bottom_nav_bar.dart
@@ -13,47 +13,55 @@ class AppBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: const Color(0xFF0C0C79),
-      unselectedItemColor: Colors.grey,
+    return Theme(
+      data: Theme.of(context).copyWith(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
 
-      onTap: onTap,
+        selectedItemColor: const Color(0xFF0C0C79),
+        unselectedItemColor: Colors.grey,
 
-      items: const [
+        elevation: 0,
+        enableFeedback: false,
 
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_outlined),
-          activeIcon: Icon(Icons.home),
-          label: "Home",
-        ),
+        onTap: onTap,
 
-        BottomNavigationBarItem(
-          icon: Icon(Icons.rss_feed_outlined),
-          activeIcon: Icon(Icons.rss_feed),
-          label: "Feed",
-        ),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: "Home",
+          ),
 
-        BottomNavigationBarItem(
-          icon: Icon(Icons.send_outlined),
-          activeIcon: Icon(Icons.send),
-          label: "Messages",
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rss_feed_outlined),
+            activeIcon: Icon(Icons.rss_feed),
+            label: "Feed",
+          ),
 
-        BottomNavigationBarItem(
-          icon: Icon(FeatherIcons.heart),
-          activeIcon: Icon(FeatherIcons.heart),
-          label: "Merch",
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.send_outlined),
+            activeIcon: Icon(Icons.send),
+            label: "Messages",
+          ),
 
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline),
-          activeIcon: Icon(Icons.person),
-          label: "Profile",
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(FeatherIcons.heart),
+            activeIcon: Icon(FeatherIcons.heart),
+            label: "Merch",
+          ),
 
-      ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
