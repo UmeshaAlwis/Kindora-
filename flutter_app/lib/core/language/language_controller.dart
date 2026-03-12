a60e7a9bf709b020bfbb85cebd26c4de29412f7a
UmeshaAlwis <uma20812ud@gmail.com>
2026-03-12 15:44:59 +0530

    Add language controller

---
+++ flutter_app/lib/core/language/language_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LanguageController extends ChangeNotifier {
+
+  Locale _locale = const Locale('en');
+
+  Locale get locale => _locale;
+
+  void changeLanguage(String code) {
+    _locale = Locale(code);
+    notifyListeners();
+  }
+
+}
